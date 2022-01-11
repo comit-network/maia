5e0493ca3537349879cf4b72935b3a54da7a56f8
Mariusz Klochowicz <mariusz@klochowicz.com>
2022-01-11 18:24:34 +1030

    Add helper to retrieve correct vout from TxOut value
    At times we don't have access to script pubkey, and need to find the correct
    vout based on the output amount.

--- src/protocol/transaction_ext.rs
+++ src/protocol/transaction_ext.rs
@@ -4,6 +4,7 @@ use bdk::bitcoin::{OutPoint, Script, Transaction};
 pub trait TransactionExt {
     fn get_virtual_size(&self) -> f64;
     fn outpoint(&self, script_pubkey: &Script) -> Result<OutPoint>;
+    fn outpoint_of_value(&self, value: u64) -> Result<OutPoint>;
 }
 
 impl TransactionExt for Transaction {
@@ -17,6 +18,19 @@ impl TransactionExt for Transaction {
             .iter()
             .position(|out| &out.script_pubkey == script_pubkey)
             .context("script pubkey not found in tx")?;
+
+        Ok(OutPoint {
+            txid: self.txid(),
+            vout: vout as u32,
+        })
+    }
+
+    fn outpoint_of_value(&self, value: u64) -> Result<Outpoint> {
+        let vout = self
+            .output
+            .iter()
+            .position(|out| &out.value == value)
+            .context("value not found in tx")?;
 
         Ok(OutPoint {
             txid: self.txid(),
