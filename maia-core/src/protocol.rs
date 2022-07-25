4b491942bc03771e15fcbd81cdd14075db8334bf
Philipp Hoenisch <philipp@coblox.tech>
2022-07-25 11:02:38 +1000

    Satisfy clippy

--- maia-core/src/protocol.rs
+++ maia-core/src/protocol.rs
@@ -91,7 +91,7 @@ impl PartialEq for Announcement {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Payout {
     digits: interval::Digits,
     maker_amount: Amount,
